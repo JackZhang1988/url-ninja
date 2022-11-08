f27ebcad7f9cbf5c5136a5053117cb3077f70cc0
jianbozhang <jianbo1988@gmail.com>
2022-11-08 08:29:55 +0800

    feat: add mui component library

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,36 @@
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import TextField from "@mui/material/TextField"
+import Grid from "@mui/material/Unstable_Grid2"
+import MenuItem from '@mui/material/MenuItem';
 import { useState } from "react"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <Box sx={{ padding: "14px", minWidth: 600 }}>
+      <Grid container spacing={1} alignItems="center">
+        <Grid xs={4}>
+          <span className="type-title">Protocol</span>
+        </Grid>
+        <Grid xs={8}>
+          <TextField
+            id="sroptocalSelect"
+            variant="standard"
+            size="small"
+            select
+            fullWidth 
+          >
+            <MenuItem key="https" value="https">https</MenuItem>
+            <MenuItem key="http" value="http">http</MenuItem>
+          </TextField>
+        </Grid>
+      </Grid>
+      <Button variant="contained" size="small">
+        Open
+      </Button>
+    </Box>
   )
 }
 
